afdcbca9b9a7acf9823657167d0d77dcf3535ede
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2024-12-13 23:02:59 -0500

    create Navbar component. Cut navbar code into this file

---
+++ 01-frontend/react-library/src/components/Navbar.tsx
@@ -0,0 +1,36 @@
+export default function Navbar(){
+    return (
+        <nav className='navbar navbar-light navbar-expand-lg py-0'>
+        <div className='container-fluid'>
+          <img className='nav-logo d-inline-block px-4' src="/main-ave-logo.svg" alt="" />
+          <button 
+            className='navbar-toggler' 
+            type='button' 
+            data-bs-toggle='collapse' 
+            data-bs-target='#navbarNavDropdown' 
+            aria-controls='navbarNavDropdown'
+            aria-expanded='false'
+            aria-label='Toggle Navigation'
+          >
+            <span className='navbar-toggler-icon'></span>
+          </button>
+          <div className='collapse navbar-collapse' id='navbarNavDropdown'>
+            <ul className='navbar-nav'>
+              <li className='nav-item'>
+                <a className='nav-link ' href="#">Home</a>
+              </li>
+              <li className='nav-item'>
+                <a className='nav-link' href="#">Search Books</a>
+              </li>
+            </ul>
+
+            <ul className='navbar-nav ms-auto'>
+              <li className='nav-item m-1'>
+                <a className='btn btn-outline-dark' type='button' href="#">Sign in</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    )
+}
