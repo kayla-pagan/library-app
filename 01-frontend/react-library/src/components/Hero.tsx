e0ca72ab9caa77df79ec2ac56b6a97f1a4d7d665
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2024-12-14 23:57:03 -0500

    add bg image to hero. Style hero section

--- 01-frontend/react-library/src/components/Hero.tsx
+++ 01-frontend/react-library/src/components/Hero.tsx
@@ -5,7 +5,7 @@ export default function Hero(){
                 <div>
                     <h1 className="display-5 fw-bold">Your aventure starts here</h1>
                     <p className="col-md-8 fs-4">Turn the page to discover where you'll go next</p>
-                    <a type="button" className="btn btn-lg text-white" href="#">Explore top reads</a>
+                    <a type="button" className="btn btn-lg text-white explore-btn" href="#" style={{backgroundColor: '#0d47a1'}}>Explore top reads</a>
                 </div>
             </div>
         </div>
