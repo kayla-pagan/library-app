232875301038147a888c542e9e62f594c292f77b
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2024-12-14 23:22:55 -0500

    add hero component. add component to App.

---
+++ 01-frontend/react-library/src/components/Hero.tsx
@@ -0,0 +1,13 @@
+export default function Hero(){
+    return (
+        <div className="p-5 mb-4 bg-dark hero">
+            <div className="container-fluid py-5 text-white d-flex justify-content-center align-items-center">
+                <div>
+                    <h1 className="display-5 fw-bold">Your aventure starts here</h1>
+                    <p className="col-md-8 fs-4">Turn the page to discover where you'll go next</p>
+                    <a type="button" className="btn btn-lg text-white" href="#">Explore top reads</a>
+                </div>
+            </div>
+        </div>
+    )
+}
