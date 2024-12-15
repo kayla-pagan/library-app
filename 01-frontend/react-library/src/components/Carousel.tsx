b10c519c53007f27b2b9eea50942d6e507dcc1e1
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2024-12-15 01:31:08 -0500

    create and style carousel. add book image to assets

---
+++ 01-frontend/react-library/src/components/Carousel.tsx
@@ -0,0 +1,131 @@
+import tiffanyBookImg from "../assets/book-tiffany-frank.svg";
+
+export default function Carousel() {
+  return (
+    <div className="container mt-5" style={{ height: 550 }}>
+      <div className="home-carousel-title text-center">
+        <h3>Find your next favorite read here.</h3>
+      </div>
+
+      <div
+        id="carouselExamples"
+        className="carousel carousel-dark slide mt-5 d-none d-lg-block"
+        data-bs-interval="false"
+      >
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <div className="row d-flex justify-content-center align-items-center">
+              <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
+                <div className="text-center d-flex flex-column align-items-center gap-3">
+                  <img
+                    src={tiffanyBookImg}
+                    alt="book"
+                    width={151}
+                    height={233}
+                  />
+                  <a
+                    className="btn text-white"
+                    style={{ backgroundColor: "#0d47a1" }}
+                    href="#"
+                  >
+                    Reserve
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="carousel-item">
+            <div className="row d-flex justify-content-center align-items-center">
+              <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
+                <div className="text-center d-flex flex-column align-items-center gap-3">
+                  <img
+                    src={tiffanyBookImg}
+                    alt="book"
+                    width={151}
+                    height={233}
+                  />
+                  <a
+                    className="btn text-white"
+                    style={{ backgroundColor: "#0d47a1" }}
+                    href="#"
+                  >
+                    Reserve
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="carousel-item">
+            <div className="row d-flex justify-content-center align-items-center">
+              <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
+                <div className="text-center d-flex flex-column align-items-center gap-3">
+                  <img
+                    src={tiffanyBookImg}
+                    alt="book"
+                    width={151}
+                    height={233}
+                  />
+                  <a
+                    className="btn text-white"
+                    style={{ backgroundColor: "#0d47a1" }}
+                    href="#"
+                  >
+                    Reserve
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExamples"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExamples"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+
+      {/* {mobile} */}
+      <div className="d-lg-none mt-3">
+        <div className="row d-flex justify-content-center align-items-center">
+          <div className="text-center">
+            <img src={tiffanyBookImg} alt="book" width={151} height={233} />
+            <a
+              className="btn text-white"
+              style={{ backgroundColor: "#0d47a1" }}
+              href="#"
+            >
+              Reserve
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="home-carousel-title mt-3 text-center">
+        <a className="btn btn-outline-secondary btn-lg" href="#">
+          View more
+        </a>
+      </div>
+    </div>
+  );
+}
