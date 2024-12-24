d4fdc4df5424f670fe00825d7b04d6cb89a00f05
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2024-12-23 20:47:29 -0500

    create backend template with dependencies in spring initializer. Add to backend folder.

---
+++ 02-backend/spring-boot-library/src/main/java/com/mainave/spring_boot_library/SpringBootApplicationForReactApplication.java
@@ -0,0 +1,13 @@
+package com.mainave.spring_boot_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApplicationForReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApplicationForReactApplication.class, args);
+	}
+
+}
---
+++ 02-backend/spring-boot-library/src/test/java/com/mainave/spring_boot_library/SpringBootApplicationForReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mainave.spring_boot_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApplicationForReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
