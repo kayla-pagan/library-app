43d420fc733acd99ac962b7a36af2b4859de668e
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2024-12-24 14:33:31 -0500

    make database read only to users

---
+++ 02-backend/spring-boot-library/src/main/java/com/mainave/spring_boot_library/config/MyDataRestConfig.java
@@ -0,0 +1,35 @@
+package com.mainave.spring_boot_library.config;
+
+import com.mainave.spring_boot_library.entity.Book;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry corsRegistry) {
+        HttpMethod[] unsupportedActions = {
+                HttpMethod.POST,
+                HttpMethod.PUT,
+                HttpMethod.DELETE,
+                HttpMethod.PATCH };
+
+        config.exposeIdsFor(Book.class);
+
+        disableHttpMethods(Book.class, config, unsupportedActions);
+
+        // Configure CORS Mapping
+        String allowedOrigins = "http://localhost:3000";
+        corsRegistry.addMapping(config.getBasePath() + "/**").allowedOrigins(allowedOrigins);
+    }
+
+    private void disableHttpMethods(Class myClass, RepositoryRestConfiguration config, HttpMethod[] unsupportedActions) {
+        config.getExposureConfiguration()
+                .forDomainType(myClass).withItemExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions));
+    }
+}
