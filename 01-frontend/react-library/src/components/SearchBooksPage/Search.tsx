6313c023267b5ad90d5fae0b1ec91c115ed6d7bd
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2024-12-25 20:46:22 -0500

    add mobile view to searchbooks page

---
+++ 01-frontend/react-library/src/components/SearchBooksPage/Search.tsx
@@ -0,0 +1,53 @@
+import BookModel from "../../models/BookModels";
+import tiffanyBookImg from "../../assets/book-tiffany-frank.svg";
+
+interface BookProps {
+  book: BookModel;
+}
+
+export default function Search({ book }: BookProps): React.ReactElement {
+  const { title, author, description, img } = book;
+
+  return (
+    <div className="card mt-3 shadow p-3 mb-3 bg-body rounded">
+      <div className="row g-0">
+        <div className="col-md-2">
+          <div className="d-none d-lg-block">
+            {img ? (
+              <img src={img} width={123} height={196} alt="Book" />
+            ) : (
+              <img src={tiffanyBookImg} width={123} height={196} alt="Book" />
+            )}
+          </div>
+        </div>
+
+        {/* mobile */}
+        <div className="d-lg-none d-flex justify-content-center align-items-center">
+          {img ? (
+            <img src={img} width={123} height={196} alt="Book" />
+          ) : (
+            <img src={tiffanyBookImg} width={123} height={196} alt="Book" />
+          )}
+        </div>
+
+        <div className="col-md-6">
+          <div className="card-body">
+            <h5 className="card-title">{author}</h5>
+            <h4>{title}</h4>
+            <p className="card-text">{description}</p>
+          </div>
+        </div>
+
+        <div className="col-md-4 d-flex justify-content-center align-items-center">
+          <a
+            className="btn btn-md text-white"
+            style={{ backgroundColor: "#0d47a1" }}
+            href="#"
+          >
+            View Details
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
