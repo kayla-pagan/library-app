f86bd325186d4dfa289e7bb1815510852e6e0ce4
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2024-12-31 17:42:01 -0500

    create okta config file. download okta dependencies

---
+++ 01-frontend/react-library/src/lib/oktaConfig.ts
@@ -0,0 +1,8 @@
+export const oktaConfig = {
+    clientId: '0oam97moqjb0yGody5d7',
+    issuer: 'https://dev-18268144.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:5173/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+    pkce: true,
+    disableHttpsCheck: true,
+}
