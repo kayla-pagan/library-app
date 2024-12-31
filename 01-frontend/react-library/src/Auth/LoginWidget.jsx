09501089388d994dd36dc672e00740dd43d396c9
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2024-12-31 17:53:19 -0500

    create auth directory. create Login Widget.

---
+++ 01-frontend/react-library/src/Auth/LoginWidget.jsx
@@ -0,0 +1,21 @@
+import { useNavigate } from "react-router-dom";
+import { useOktaAuth } from "@okta/okta-react";
+import SpinnerLoading from "../utils/SpinnerLoading"
+
+export default function LoginWidget({ config }){
+    const { oktaAuth, authState } = useOktaAuth()
+    const onSuccess = (tokens) => oktaAuth.handleLoginRedirect(tokens)
+    const onError = (err) => console.log("Sign in error: ", err)
+    const navigate = useNavigate()
+
+    if(!authState) {
+        return (
+            <SpinnerLoading />
+        )
+    }
+
+    return authState.isAuthenticated ? 
+    navigate("/", {replace: true})
+    :
+    <div></div>
+}
