7df22730a6e2622ed066724c979806a47b63bfbe
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2024-12-31 19:04:08 -0500

    add okta configuration to app component

--- 01-frontend/react-library/src/App.tsx
+++ 01-frontend/react-library/src/App.tsx
@@ -1,11 +1,23 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import SearchBooks from "./pages/SearchBooks";
 import Layout from "./components/Layout";
 import CheckoutBook from "./pages/CheckoutBook";
+import { oktaConfig } from "./lib/oktaConfig";
+import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
+
+const oktaAuth = new OktaAuth(oktaConfig)
 
 function App() {
+  const navigate = useNavigate()
+  const handleAuth = () => navigate("/login")
+  const restoreOriginalUri = async () => {
+    const relativeUrl = toRelativeUrl(originalUri || "/", window.location.origin)
+    navigate(relativeUrl, { replace: true })
+  }
+
+
   return (
     <div className="d-flex flex-column min-vh-100">
         <Routes>
