60aea09335652580d00050508fa151f5e3b21e27
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2024-12-31 23:28:04 -0500

    create controller to make book service usable via mapping

---
+++ 02-backend/spring-boot-library/src/main/java/com/mainave/spring_boot_library/controller/BookController.java
@@ -0,0 +1,24 @@
+package com.mainave.spring_boot_library.controller;
+
+import com.mainave.spring_boot_library.entity.Book;
+import com.mainave.spring_boot_library.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:5173")
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+    private final BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PutMapping("/secure/checkout")
+    public Book checkoutBook(@RequestParam Long bookId) throws Exception {
+        String userEmail = "testuser@email.com";
+        return bookService.checkoutBook(userEmail, bookId);
+    }
+}
