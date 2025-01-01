12c52232d768056c4ca2e7e3169d7fb20aee6fd2
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2024-12-31 23:49:56 -0500

    add mapping for how many books a user has checked out and if a book is checked out by the user or not

--- 02-backend/spring-boot-library/src/main/java/com/mainave/spring_boot_library/dao/CheckoutRepository.java
+++ 02-backend/spring-boot-library/src/main/java/com/mainave/spring_boot_library/dao/CheckoutRepository.java
@@ -2,7 +2,10 @@ package com.mainave.spring_boot_library.dao;
 
 import com.mainave.spring_boot_library.entity.Checkout;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface CheckoutRepository extends JpaRepository<Checkout, Long> {
     Checkout findByUserEmailAndBookId(String userEmail, Long bookId);
+
+    List<Checkout> findBooksByUserEmail(String userEmail);
 }
--- 02-backend/spring-boot-library/src/main/java/com/mainave/spring_boot_library/service/BookService.java
+++ 02-backend/spring-boot-library/src/main/java/com/mainave/spring_boot_library/service/BookService.java
@@ -44,4 +44,14 @@ public class BookService {
 
         return book.get();
     }
+
+    public boolean checkoutBookByUser(String userEmail, Long bookId) {
+        Checkout validateCheckout = checkoutRepository.findByUserEmailAndBookId(userEmail, bookId);
+
+        return validateCheckout != null;
+    }
+
+    public int currentLoansCount(String userEmail) {
+        return checkoutRepository.findBooksByUserEmail(userEmail).size();
+    }
 }
