0d93b91f79cf8c933378365c34beaf3ca33c7736
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2024-12-31 22:45:57 -0500

    add rest repository resource annotation over review repo

--- 02-backend/spring-boot-library/src/main/java/com/mainave/spring_boot_library/dao/ReviewRepository.java
+++ 02-backend/spring-boot-library/src/main/java/com/mainave/spring_boot_library/dao/ReviewRepository.java
@@ -4,8 +4,10 @@ import com.mainave.spring_boot_library.entity.Review;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@RepositoryRestResource
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     Page<Review> findByBookId(@RequestParam("book_id") Long bookId, Pageable pageable);
 }
