4b383aa78a5128fe2ca8403ffa454f0b894d5faf
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2025-01-05 19:24:27 -0500

    create message controller with post message endpoint

---
+++ 02-backend/spring-boot-library/src/main/java/com/mainave/spring_boot_library/controller/MessageController.java
@@ -0,0 +1,26 @@
+package com.mainave.spring_boot_library.controller;
+
+import com.mainave.spring_boot_library.entity.Message;
+import com.mainave.spring_boot_library.service.MessageService;
+import com.mainave.spring_boot_library.utils.ExtractJWT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:5173")
+@RestController
+@RequestMapping("/api/messages")
+public class MessageController {
+    private final MessageService messageService;
+
+    @Autowired
+    public MessageController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @PostMapping("/secure/add/message")
+    public void postMessage(@RequestHeader(value = "Authorization") String token,
+                            @RequestBody Message messageRequest) {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        messageService.postMessage(messageRequest, userEmail);
+    }
+}
