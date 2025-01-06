a8e49c9f6ed499f5593d12dd75cb576fdb273f9b
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2025-01-05 19:18:20 -0500

    create message service

---
+++ 02-backend/spring-boot-library/src/main/java/com/mainave/spring_boot_library/service/MessageService.java
@@ -0,0 +1,25 @@
+package com.mainave.spring_boot_library.service;
+
+import com.mainave.spring_boot_library.dao.MessageRepository;
+import com.mainave.spring_boot_library.entity.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class MessageService {
+
+    private MessageRepository messageRepository;
+
+    @Autowired
+    public MessageService(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+    public void postMessage(Message messageRequest, String userEmail) {
+        Message message = new Message(messageRequest.getTitle(), messageRequest.getQuestion());
+        message.setUserEmail(userEmail);
+        messageRepository.save(message);
+    }
+}
