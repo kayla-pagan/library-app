f18ee670b6b39b4b3290d88f1952ef1b17f37b5b
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2025-01-05 20:26:07 -0500

    create message model

---
+++ 01-frontend/react-library/src/models/MessageModel.ts
@@ -0,0 +1,21 @@
+export class MessageModel {
+    title: string;
+    question: string;
+    id?: number;
+    userEmail?: string;
+    adminEmail?: string;
+    response?: string;
+    closed?: boolean;
+
+    constructor(
+        title: string, question: string, id: number, userEmail: string, 
+        adminEmail: string, response: string, closed: boolean){
+            this.title = title;
+            this.question = question;
+            this.id = id;
+            this.userEmail = userEmail;
+            this.adminEmail = adminEmail;
+            this.response = response;
+            this.closed = closed;
+        }
+}
