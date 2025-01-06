30388fbe280c55d955a82b74c79ee23bb1d961d9
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2025-01-05 19:14:07 -0500

    create message repository

---
+++ 02-backend/spring-boot-library/src/main/java/com/mainave/spring_boot_library/dao/MessageRepository.java
@@ -0,0 +1,6 @@
+package com.mainave.spring_boot_library.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MessageRepository  extends JpaRepository<Message, Long> {
+}
