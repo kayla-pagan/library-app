4de76f77081618ef0b5a36506b779aa42afe10ed
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2025-01-08 15:57:59 -0500

    create service to generate a url when admin uploads a picture

--- 02-backend/spring-boot-library/src/main/java/com/mainave/spring_boot_library/service/AdminService.java
+++ 02-backend/spring-boot-library/src/main/java/com/mainave/spring_boot_library/service/AdminService.java
@@ -1,5 +1,8 @@
 package com.mainave.spring_boot_library.service;
 
+import com.amazonaws.HttpMethod;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
 import com.mainave.spring_boot_library.dao.BookRepository;
 import com.mainave.spring_boot_library.entity.Book;
 import com.mainave.spring_boot_library.requestmodels.AddBookRequest;
@@ -7,15 +10,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.net.URL;
+import java.util.Date;
+
 @Service
 @Transactional
 public class AdminService {
 
     private BookRepository bookRepository;
 
+    private AmazonS3 amazonS3;
+
     @Autowired
-    public AdminService(BookRepository bookRepository) {
+    public AdminService(BookRepository bookRepository, AmazonS3 amazonS3) {
         this.bookRepository = bookRepository;
+        this.amazonS3 = amazonS3;
     }
 
     public void postBook(AddBookRequest addBookRequest){
@@ -28,5 +37,17 @@ public class AdminService {
         book.setCategory(addBookRequest.getCategory());
         book.setImg(addBookRequest.getImg());
         bookRepository.save(book);
+    }
+
+    public String generatePresignedUrl(String fileName, String fileType) {
+        Date expiration = new Date(System.currentTimeMillis() + 3600 * 1000);
+        GeneratePresignedUrlRequest generatePresignedUrlRequest = new GeneratePresignedUrlRequest(
+                "main-ave-book-images", fileName)
+                    .withMethod(HttpMethod.PUT)
+                    .withExpiration(expiration)
+                    .withContentType(fileType);
+
+        URL url = amazonS3.generatePresignedUrl(generatePresignedUrlRequest);
+        return url.toString();
     }
 }
