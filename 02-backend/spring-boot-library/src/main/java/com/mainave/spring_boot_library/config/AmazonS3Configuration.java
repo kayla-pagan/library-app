69523f49a63f426dd79e12cbdefdcd256844cf31
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2025-01-12 13:19:03 -0500

    complete s3 set up and related methods for admin image upload

--- 02-backend/spring-boot-library/src/main/java/com/mainave/spring_boot_library/config/AmazonS3Configuration.java
+++ 02-backend/spring-boot-library/src/main/java/com/mainave/spring_boot_library/config/AmazonS3Configuration.java
@@ -3,10 +3,12 @@ package com.mainave.spring_boot_library.config;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AmazonS3Configuration {
+    @Bean
     public AmazonS3 amazonS3() {
         return AmazonS3ClientBuilder.standard()
                 .withRegion("us-east-1")
